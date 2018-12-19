de81c2fe11156ae9a01b2c0c38c202f6aadaab58
Artur Debski <xmentero@gmail.com>
2018-12-19 13:26:30 +0100

    3a

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,13 @@
 const sum = require('./sum');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  // Arrange
+  a = 1
+  b = 2
+
+  // Act
+  result = sum(a, b)
+
+  // Assert
+  expect(result).toBe(3);
 });
